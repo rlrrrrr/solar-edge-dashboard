1b787619c45ccd3c9f5bc4b777667a7a4ce33c2d
Hugo DESSAUD <hugo.dessaud@etu.univ-amu.fr>
2024-05-22 15:50:26 +0200

    Linking and minor changes

--- front/vite-project/app/routes/_index.tsx
+++ front/vite-project/app/routes/_index.tsx
@@ -18,8 +18,8 @@ export default function Home() {
         <Section
             btnValueBlack="En savoir plus"
             btnValueWhite="Nous contacter"
-            blackLink="#"
-            whiteLink="#"
+            blackLink="/presentation"
+            whiteLink="/contact"
             text="Un projet novateur situé sur le campus de l'IUT de St Jérôme à Marseille. Créé en 2021 grâce à un financement de plus de 300 000 € d'AMideX et à l'implication de cinq départements de l'IUT (TC, MP, GEII Salon, GEII Marseille et R&T), ce magasin est une plateforme technologique avant-gardiste de 150 m² dédiée aux étudiants. C'est un véritable laboratoire grandeur nature où les étudiants peuvent installer et étudier diverses technologies innovantes. Le projet phare du Magasin Connecté est le MEPOS (Magasin Connecté à Énergie Positive), visant à rendre la boutique autonome en énergie grâce à l'installation de panneaux solaires sur son toit. Ces panneaux permettront de collecter des données précieuses sur la production et la consommation d'énergie, contribuant ainsi à la recherche et à la sensibilisation aux énergies renouvelables. Le Magasin Connecté accueille également une épicerie solidaire, gérée par une association étudiante indépendante de l'IUT, qui offre des produits essentiels à des prix réduits pour soutenir les étudiants dans le besoin."
             title="Bienvenue au Magasin Connecté 4.0"
             imgSrc="https://images-ext-1.discordapp.net/external/VLQJbkqc9sUkkZdn5Hti6Qnzqt1kZlhcneq34gaZ3w4/%3Fq%3D80%26w%3D3087%26auto%3Dformat%26fit%3Dcrop%26ixlib%3Drb-4.0.3%26ixid%3DM3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%253D%253D/https/images.unsplash.com/photo-1715622053361-4baf5ad2c51a?format=webp&width=376&height=565"
@@ -63,10 +63,10 @@ export default function Home() {
         <PartnerSection />
 
         <Section
-            btnValueBlack="En savoir plus"
+            btnValueBlack="Nous rendre visite"
             btnValueWhite="Nous contacter"
-            blackLink="#"
-            whiteLink="#"
+            blackLink="/presentation"
+            whiteLink="/contact"
             text="Nous vous invitons chaleureusement à venir découvrir le Magasin Connecté sur le campus de l'IUT de St Jérôme à Marseille. Plongez dans un univers technologique avant-gardiste où innovation et durabilité se rencontrent. Explorez notre plateforme, interagissez avec nos installations high-tech, et découvrez comment nos étudiants façonnent l'avenir des technologies et des énergies renouvelables. Nous serons ravis de vous accueillir et de partager avec vous notre passion pour l'innovation et la durabilité."
             title="Venez Nous Rendre Visite"
             imgSrc="https://images-ext-1.discordapp.net/external/VLQJbkqc9sUkkZdn5Hti6Qnzqt1kZlhcneq34gaZ3w4/%3Fq%3D80%26w%3D3087%26auto%3Dformat%26fit%3Dcrop%26ixlib%3Drb-4.0.3%26ixid%3DM3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%253D%253D/https/images.unsplash.com/photo-1715622053361-4baf5ad2c51a?format=webp&width=376&height=565"
--- front/vite-project/components/ui/title.tsx
+++ front/vite-project/components/ui/title.tsx
@@ -3,9 +3,10 @@ import React from "react";
 interface TitleProps {
   variant?: 'primary' | 'secondary' | 'tertiary';
   children?: React.ReactNode;
+  supClass?: string;
 }
 
-export default function Title({ variant = 'primary', children }: TitleProps) {
+export default function Title({ variant = 'primary', children, supClass }: TitleProps) {
   const baseClass = "tracking-tight";
   const variantClass = {
     primary: "text-5xl font-bold",
@@ -13,7 +14,7 @@ export default function Title({ variant = 'primary', children }: TitleProps) {
     tertiary: "text-xl font-medium",
   };
 
-  const className = `${baseClass} ${variantClass[variant]}`;
+  const className = `${supClass} ${baseClass} ${variantClass[variant]}`;
 
   return <h1 className={className}>{children}</h1>;
 }
