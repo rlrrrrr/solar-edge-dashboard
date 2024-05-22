871f4b9e8e3edac09a1b027cf70b2238258a151b
Hugo DESSAUD <hugo.dessaud@etu.univ-amu.fr>
2024-05-22 15:50:26 +0200

    Linking and minor changes

--- front/vite-project/app/routes/solarPanelDashboard.tsx
+++ front/vite-project/app/routes/solarPanelDashboard.tsx
@@ -5,8 +5,8 @@ import Paragraph from "components/ui/paragraph"
 export default function Component() {
     return (
         <>
-            <HeroSection />
             <div className="flex min-h-screen flex-col">
+              <Title supClass="mb-10">Dashboard : panneaux solaires</Title>
                 <main className="flex-1 bg-gray-100 py-8 px-6">
                     <div className="container mx-auto grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
                         <DashboardCard title="graphe 1">
