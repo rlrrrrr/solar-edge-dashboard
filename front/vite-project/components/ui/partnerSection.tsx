23a2914a43dca11333fcc855dbdfe56e164ad27d
Hugo DESSAUD <hugo.dessaud@etu.univ-amu.fr>
2024-05-22 13:26:27 +0200

    theme adjustments

--- front/vite-project/components/ui/partnerSection.tsx
+++ front/vite-project/components/ui/partnerSection.tsx
@@ -3,7 +3,7 @@ import Title from "./title"
 
 export default function PartnerSection(){
   return (
-    <section className="w-full py-12 md:py-24 lg:py-32">
+    <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-100 dark:bg-gray-800">
         <div className="container grid items-center justify-center gap-4 px-4 text-center md:px-6 lg:gap-10">
           <div className="space-y-3">
             <Title variant='primary'>Nos partenaires</Title>
