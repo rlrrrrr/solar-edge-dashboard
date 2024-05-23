79792166dd947ebc71298a990336df15bca0d608
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2024-05-23 11:15:14 +0200

    fix: reset cache every 15 minutes when asking for live electricity data

--- back/app/controllers/api_controller.ts
+++ back/app/controllers/api_controller.ts
@@ -1,18 +1,36 @@
 import { HttpContext } from '@adonisjs/core/http'
 import env from '#start/env'
 import logger from '@adonisjs/core/services/logger';
+import { log } from 'console';
 
 const timeUnits = ["QUARTER_OF_AN_HOUR", "HOUR", "DAY", "WEEK", "MONTH", "YEAR"];
 
 type CacheKeyStr = string;
 class CacheKey {
-  public startDate: string;
-  public endDate: string;
-  public timeUnit: string;
+  private startDate: string;
+  private endDate: string;
+  private timeUnit: string;
   constructor(startDate: string, endDate: string, timeUnit: string) {
     this.startDate = startDate;
-    this.endDate = endDate;
     this.timeUnit = timeUnit;
+
+    let currentDay = new Date().toISOString().split('T')[0];
+    let endDay = new Date(endDate).toISOString().split('T')[0];
+
+    if (endDay >= currentDay) { // yes, we can compare date strings like that
+      logger.debug("End date is today or in the future, meaning we want live data. Setting cache key to reset at the next 15 minutes marker");
+
+      // update time to the next 15 minutes. e.g. 11:31:00 -> 11:45:00
+      let flooredTime = new Date();
+      let flooredMinutes = Math.floor(flooredTime.getMinutes() / 15) * 15;
+      flooredTime.setMinutes(flooredMinutes);
+      flooredTime.setSeconds(0);
+      flooredTime.setMilliseconds(0);
+
+      this.endDate = flooredTime.toISOString();
+    } else {
+      this.endDate = endDate;
+    }
   }
 
   toString(): CacheKeyStr {
@@ -44,8 +62,11 @@ export default class ApiController {
 
     // caclulate cache key
     let timeUnit = ensureParam('timeUnit');
+    let startDate = ensureParam('startDate');
+    let endDate = ensureParam('endDate');
     if (!timeUnits.includes(timeUnit)) response.abort({ message: "Invalid time unit: "+timeUnit})
-    let key = new CacheKey(ensureParam('startDate'), ensureParam("endDate"), timeUnit);
+    let key = new CacheKey(startDate, endDate, timeUnit);
+    console.log(key.toString());
 
     // check if we have cached this request in the cache
     let value = cache.get(key.toString());
@@ -55,13 +76,14 @@ export default class ApiController {
       return;
     }
 
-    logger.info("Sending SolarEdge request for key "+key.toString());
+    logger.info(`Sending SolarEdge request with start date ${startDate}, end date ${endDate}, time unit ${timeUnit}`);
+    logger.info(`(Wanted key is ${key.toString()})`);
     // make request to SolarEdge API
     let resp = await fetch(endpoint + `/site/${siteId}/energy?`+new URLSearchParams({
       api_key: apiKey!,
-      startDate: key.startDate,
-      endDate: key.endDate,
-      timeUnit: key.timeUnit,
+      startDate: startDate,
+      endDate: endDate,
+      timeUnit: timeUnit,
     }));
 
     if (!resp.ok) {
