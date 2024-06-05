9afbc185c91217b5b72d6aca1a884cd860fd48ac
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2024-06-05 16:11:10 +0200

    add /calendar GET and POST

---
+++ back/database/migrations/1717595582373_create_variables_table.ts
@@ -0,0 +1,16 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'variables'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.string('key').notNullable().unique()
+      table.string('value').notNullable()
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
