f19efefcb9e6ed51ecd88b8e8b611d82a7f3f28b
Romain CLEMENT <rc2301@orange.fr>
2024-06-06 11:57:09 +0200

    Added vertical line to show current time

--- front/components/ui/dayProductionChart.tsx
+++ front/components/ui/dayProductionChart.tsx
@@ -1,12 +1,41 @@
-import { useState, useEffect } from 'react'; // Importez useEffect
+import { useEffect, useState } from 'react'; // Importez useEffect
 import ChartComponent from '../../components/ui/chart';
-import {format} from "date-fns";
+import { format } from 'date-fns';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+import annotationPlugin from "chartjs-plugin-annotation";
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+    annotationPlugin
+);
 
 export default function DayProductionChart({data}) {
     const [chartData, setChartData] = useState({
         labels: [],
         datasets: []
     });
+
+    const currentDate = format(new Date(), 'yyyy-MM-dd');
+    const currentHour = new Date();
+    let flooredMinutes = Math.floor(currentHour.getMinutes() / 15) * 15;
+    currentHour.setMinutes(flooredMinutes);
 
     useEffect(() => {
         const newData = {
@@ -44,12 +73,27 @@ export default function DayProductionChart({data}) {
         plugins: {
             legend: {
                 display: false
+            },
+            annotation: {
+                annotations: {
+                    now: {
+                        type: 'line',
+                        scaleID: 'x',
+                        value: format(currentHour, 'HH:mm'),
+                        borderColor: "red",
+                        label: {
+                            display: true,
+                            position: 'start',
+                            content: 'Maintenant'
+                        }
+                    }
+                }
             }
         }
     };
 
     return (
-        <ChartComponent
+        <Line
             id="energyDayProductionChart"
             chartType="line"
             data={chartData}
