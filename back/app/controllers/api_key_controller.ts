5ae09bd1570687e294386fa2a94c7a3c2334dd9b
rlrr <riyad.larbaoui@kpconsulting.fr>
2024-06-07 15:52:40 +0200

    rename api_key controller

--- back/app/controllers/api_key_controller.ts
+++ back/app/controllers/api_key_controller.ts
@@ -1,7 +1,7 @@
 import { HttpContext } from '@adonisjs/core/http'
 import env from '#start/env'
 
-export default class AdminController {
+export default class ApiKeyController {
   async setSolarEdgeKey({ request, response }: HttpContext) {
     let {key} = request.body();
     if (!key) response.abort({ message: "Missing parameter: key" });
